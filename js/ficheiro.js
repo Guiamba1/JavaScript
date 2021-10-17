7c84ed67a3640884628f99e521ac4da5461b5c80
Oscar Alberto Guiamba <oscarguiamba.a@gmail.com>
2021-10-17 08:33:43 -0700

    teste

--- js/ficheiro.js
+++ js/ficheiro.js
@@ -8,8 +8,8 @@ let jogando;
 //Constantes
 const wordinput = document.querySelector('#wordinput');
 const curentword = document.querySelector('#curentword');
-const scoreDisplay = document.querySelector('#score');
-const timeDisplay = document.querySelector('#time');
+const scoreDisplay = document.querySelector('#pontos');
+const timeDisplay = document.querySelector('#tempo');
 const segundos = document.querySelector('#segundos');
 const messagem = document.querySelector('#messagem');
 
@@ -21,10 +21,63 @@ const palavras = [
 ];
 
 function iniciar(){
-    showWords(palavras);
+    showWords(palavras);//Mostra aleatoriamente as palavras
+
+    setInterval(regressiva,1000);//Contagem regressiva
+
+    setInterval(estado,50); //Estado do Jogo
+
+    wordinput.addEventListener('input', comparar);
+}
+//=================================================
+function comparar(){
+    if (compararPalavras()){
+       
+        console.log('fuciona');
+        jogando = true;
+        tempo = 6;
+        showWords(palavras);
+        wordinput.value = ''; 
+        pontos++;
+              }
+              scoreDisplay.innerHTML = pontos;
+           }
+
+function compararPalavras(){
+    if (wordinput.value == curentword.innerHTML) {
+        messagem.innerHTML = 'certo';
+        return true;   
+    }
+    else{
+        messagem.innerHTML = '';
+        return false;
+    }
 }
 
+//Mostra aleatoriamente as palavras
 function showWords(){
     const randIndex = Math.floor(Math.random() * palavras.length);
     curentword.innerHTML = palavras[randIndex];
+}
+
+//Contagem regressiva
+function regressiva(){
+    if (tempo > 0) {
+        tempo --;
+        }
+
+    else if (tempo == 0) {
+        jogando = false;
+        }
+        timeDisplay.innerHTML = tempo;
+    }
+    
+
+
+////Estado do Jogo
+function estado(){
+    if (!jogando && tempo == 0) {
+        messagem.innerHTML = 'game Over'; 
+        
+    }
 }
